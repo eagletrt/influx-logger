b6ffeaf1efe33d802353e8c24a3d6421d20dd265
ozneroL541 <l.21radice@gmail.com>
2026-05-27 23:05:40 +0200

    feat: simplify run_test.sh to restart src.main every 30 minutes

--- run_test.sh
+++ run_test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-finished=1
-hard_killed=1
-
-# if Ctrl+C (SIGINT) arrives, mark as hard killed so loop can exit
-trap 'hard_killed=0' INT
-while [ "$finished" -ne 0 ] && [ "$hard_killed" -ne 0 ]; do
-    timeout -s KILL 30m python3 -m src.main config.json
-    finished=$?
+# Restart src.main every 30 minutes.
+while true; do
+    python3 -m src.main config.json &
+    pid=$!
+    sleep 1800
+    kill "$pid"
+    wait "$pid"
 done
+
