5c4a1157f0d72e0cc16e24ecfa55fb9ec95b81a1
ozneroL541 <l.21radice@gmail.com>
2026-06-02 16:57:08 +0200

    feat: enhance performance loading by adding file argument handling and error checking

--- test_report/look_at_perf.py
+++ test_report/look_at_perf.py
@@ -1,8 +1,18 @@
+import sys
+import os
 from test_report.performances import Performances
 
 
 def main():
-    performances = Performances.load_from_file()
+    args = sys.argv[1:]
+    if len(args) != 1:
+        performances = Performances.load_from_file()
+    elif os.path.isfile(args[0]):
+        print(f"Loading performances from file: {args[0]}")
+        performances = Performances.load_from_file(args[0])
+    else:
+        print(f"File {args[0]} does not exist.")
+        return
     #for name, performance in performances.performances.items():
     #    print(f"\n{name}:\n{performance}")
     #performances.plot_all(save=False, cols=4)
