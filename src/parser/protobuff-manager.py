294ec0c1329c254118af4807e067b55355ba8f65
ozneroL541 <l.21radice@gmail.com>
2026-06-29 10:22:21 +0200

    feat: mv proto functions to protobuff-manager

--- src/parser/protobuff-manager.py
+++ src/parser/protobuff-manager.py
@@ -1,11 +1,77 @@
-from os import path
+import os
+import sys
 from typing import Any
 from requests import get
+from types import ModuleType
 from tempfile import TemporaryDirectory
+from importlib.util import spec_from_loader, spec_from_file_location, module_from_spec
 
 from src.utils.logger_utils import logger
 
-# TODO: document
+# TODO: Comments and documentation
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+generated_proto_root = os.path.join(project_root, ".generated/external/serializers/proto")
+
+sys.path.insert(0, project_root)
+sys.path.insert(0, generated_proto_root)
+
+# TODO: improve readability
+def _register_generated_proto_package(package_name: str):
+    package_path = os.path.join(generated_proto_root, package_name)
+    if not os.path.isdir(package_path):
+        return
+
+    module = ModuleType(package_name)
+    module.__path__ = [package_path]
+    module.__package__ = package_name
+    module.__file__ = os.path.join(package_path, "__init__.py")
+    module.__spec__ = spec_from_loader(package_name, loader=None, is_package=True)
+    if module.__spec__ is not None:
+        module.__spec__.submodule_search_locations = [package_path]
+
+    sys.modules[package_name] = module
+
+def _load_generated_proto_module(package_name: str, module_name: str):
+    package_path = os.path.join(generated_proto_root, package_name)
+    module_path = os.path.join(package_path, f"{module_name}.py")
+    if not os.path.isfile(module_path):
+        return
+
+    full_name = f"{package_name}.{module_name}"
+    if full_name in sys.modules:
+        return
+
+    spec = spec_from_file_location(full_name, module_path)
+    if spec is None or spec.loader is None:
+        return
+
+    module = module_from_spec(spec)
+    sys.modules[full_name] = module
+    spec.loader.exec_module(module)
+    setattr(sys.modules[package_name], module_name, module)
+
+for package_name in [
+    "actions",
+    "app",
+    "can",
+    "configs",
+    "data",
+    "handcart",
+    "influxlogger",
+    "lapcounter",
+    "mongodb",
+    "sessions",
+    "telemetry",
+    "tpms",
+]:
+    _register_generated_proto_package(package_name)
+    package_path = os.path.join(generated_proto_root, package_name)
+    if os.path.isdir(package_path):
+        for file_name in os.listdir(package_path):
+            if file_name.endswith(".py") and file_name != "__init__.py":
+                _load_generated_proto_module(package_name, file_name[:-3])
+
+# TODO: documentation
 class ProtobuffManager:
     version_descriptors: dict[str, dict[str, Any]]
     ''' Version descriptors maps version -> network -> protobuf type/object'''
@@ -34,8 +100,6 @@ class ProtobuffManager:
             return
 
         logger.info("Proto: Descriptor successfully parsed and is now ready for deserialize data")
-
-
 
 class LibCANManager:
     CAN_PROTO_URL = (
@@ -79,8 +143,8 @@ class _DecoderWrapper:
             ) from e
 
         with TemporaryDirectory(prefix="influx_proto_") as tmp_dir:
-            proto_file = path.join(tmp_dir, f"{network}.proto")
-            descriptor_set_file = path.join(tmp_dir, "descriptor_set.pb")
+            proto_file = os.path.join(tmp_dir, f"{network}.proto")
+            descriptor_set_file = os.path.join(tmp_dir, "descriptor_set.pb")
 
             with open(proto_file, "w", encoding="utf-8") as fh:
                 fh.write(descriptor_raw)
