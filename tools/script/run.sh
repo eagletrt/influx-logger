893b1a2e6f0f3391a41c89dc8309caf31f91277e
ozneroL541 <l.21radice@gmail.com>
2026-08-09 14:06:30 +0200

    fix: naming

--- tools/script/run.sh
+++ tools/script/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mkdir -p ./cache
-docker run --mount=type=bind,src=$(pwd)/config.json,target=/app/config.json,readonly --mount=type=bind,src=./cache,target=/app/cache influx-logger:latest
+docker run --mount=type=bind,src=$(pwd)/config.json,target=/app/config.json,readonly --mount=type=bind,src=./cache,target=/app/cache --name=influx-logger influx-logger:latest
