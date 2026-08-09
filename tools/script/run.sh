96edb7121145a2c77ff84db669e6bee51096adaa
ozneroL541 <l.21radice@gmail.com>
2026-08-09 14:20:28 +0200

    fix: docker run

--- tools/script/run.sh
+++ tools/script/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mkdir -p ./cache
-docker run --mount=type=bind,src=$(pwd)/config.json,target=/app/config.json,readonly --mount=type=bind,src=./cache,target=/app/cache --name=influx-logger influx-logger:latest
+docker start influx-logger || docker run --mount=type=bind,src=$(pwd)/config.json,target=/app/config.json,readonly --mount=type=bind,src=./cache,target=/app/cache --name=influx-logger influx-logger:latest
