992402b25044177c91264e4fdf0b8cc95832fee6
ozneroL541 <l.21radice@gmail.com>
2026-08-09 13:50:22 +0200

    fix: makefile

--- tools/script/run.sh
+++ tools/script/run.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+mkdir -p ./cache
 docker run --mount=type=bind,src=$(pwd)/config.json,target=/app/config.json,readonly --mount=type=bind,src=./cache,target=/app/cache influx-logger:latest
