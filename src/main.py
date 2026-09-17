451585891f1f154a6b825399ac57e04867ceb586
ozneroL541 <l.21radice@gmail.com>
2026-09-17 18:23:17 +0200

    fix: unhandled exception

--- src/main.py
+++ src/main.py
@@ -27,13 +27,27 @@ def main(argv=None):
     #    logger.error(f"Failed to load configuration from {conf}: {e}")
     #    sys.exit(1)
     logger.info(f"Configuration loaded from {conf}: {configuration}")
-    try:
-        handler:HandlerFSM = HandlerFSM(configuration)
-        handler.start()
-        handler.join()
-    except KeyboardInterrupt:
-        logger.info("Ctrl+C received, stopping handler")
-        safe_stop(handler)
+    stop: bool = False
+    '''
+    Flag to restart in case of unexpected error.
+    '''
+    while not stop:
+        stop = True
+        try:
+            handler:HandlerFSM = HandlerFSM(configuration)
+            handler.start()
+            handler.join()
+        except KeyboardInterrupt:
+            logger.info("Ctrl+C received, stopping handler")
+            safe_stop(handler)
+        except Exception as e:
+            logger.error(f"HandlerFSM encountered an error: {e}")
+            try:
+                handler.stop_machine()
+                handler.join()
+            except Exception:
+                pass
+            stop = False
 
 def print_fsm():
     """
